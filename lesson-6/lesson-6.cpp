2e9a920d22a89f2e387c47b654098326446556d1
eleliauk <2831336720@qq.com>
2024-12-13 11:25:01 +0800

    feat(lesson-6) :lesson-6

---
+++ lesson-6/lesson-6.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <fstream>
+#include <unordered_map>
+#include <vector>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+unordered_map<string, string> englishToChinese;
+unordered_map<string, vector<string>> chineseToEnglish;
+
+void loadDictionary(const string &filename) {
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cerr << "无法打开文件: " << filename << endl;
+        return;
+    }
+
+    string line;
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string english, chinese;
+        size_t pos = line.find("   ");
+        if (pos != string::npos) {
+            english = line.substr(0, pos);
+            chinese = line.substr(pos + 3); 
+            englishToChinese[english] = chinese;
+            size_t start = 0, end;
+            string word = chinese.substr(start);
+            chineseToEnglish[word].push_back(english);
+        }
+    }
+
+    file.close();
+}
+
+// 查询英文对应的中文
+void queryEnglish(const string &word) {
+    if (englishToChinese.find(word) != englishToChinese.end()) {
+        cout << "中文解释: " << englishToChinese[word] << endl;
+    } else {
+        cout << "未找到英文单词: " << word << endl;
+    }
+}
+
+void queryChinese(const string &word) {
+    if (chineseToEnglish.find(word) != chineseToEnglish.end()) {
+        cout << "相关英文单词: ";
+        for (const auto &english : chineseToEnglish[word]) {
+            cout << english << " ";
+        }
+        cout << endl;
+    } else {
+        cout << "未找到中文词汇: " << word << endl;
+    }
+}
+
+int main() {
+    string filename = "vocabulary.txt"; 
+    loadDictionary(filename);
+
+    cout << "英汉-汉英字典程序启动！" << endl;
+    while (true) {
+        cout << "输入单词进行查询（输入 'exit' 退出）：";
+        string input;
+        cin >> input;
+        if (input == "exit") break;
+        if (isalpha(input[0])) {
+            queryEnglish(input);
+        } else {
+            queryChinese(input);
+        }
+    }
+
+    cout << "程序退出。" << endl;
+    return 0;
+}
